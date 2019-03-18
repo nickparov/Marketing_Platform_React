50c01c5133d3781b2c2c86ad508b0b9d4904e976
elddiablo <elddiablo001@gmail.com>
2019-03-17 19:53:57 -0500

    Updates

---
+++ precache-manifest.b728525e1c5a42601224cebc26f738a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "133a532af5c539a48d1f2aad3ac379fd",
+    "url": "/Marketing_Platform_React/static/media/banner.133a532a.jpg"
+  },
+  {
+    "revision": "ba5f65be6c3fa8b45b55",
+    "url": "/Marketing_Platform_React/static/js/runtime~main.ba5f65be.js"
+  },
+  {
+    "revision": "d0a6bc83ac652fdfb505",
+    "url": "/Marketing_Platform_React/static/js/main.d0a6bc83.chunk.js"
+  },
+  {
+    "revision": "1bca2d714c169a2b0918",
+    "url": "/Marketing_Platform_React/static/js/4.1bca2d71.chunk.js"
+  },
+  {
+    "revision": "9e132b5f78bc4813f14b",
+    "url": "/Marketing_Platform_React/static/js/3.9e132b5f.chunk.js"
+  },
+  {
+    "revision": "c14e85d249218ccb006c",
+    "url": "/Marketing_Platform_React/static/js/2.c14e85d2.chunk.js"
+  },
+  {
+    "revision": "d0a6bc83ac652fdfb505",
+    "url": "/Marketing_Platform_React/static/css/main.0fe9475a.chunk.css"
+  },
+  {
+    "revision": "e7e1ce9dc10be5bc24143c06dce119c2",
+    "url": "/Marketing_Platform_React/index.html"
+  }
+];
